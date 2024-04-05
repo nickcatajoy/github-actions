03faf0d40f0cb0276ad56393479a8bef862b0ff3
catajoynick <128123676+nickcatajoy@users.noreply.github.com>
2024-04-05 13:09:03 +0800

    hello world!
    hello from the other side.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => res.status(200).json({ data: "OK" }))
+app.get('/', (req, res) => res.status(200).json({ data: "Updating from the other side." }))
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
